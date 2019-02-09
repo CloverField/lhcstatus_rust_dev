666da390a3419b1e7cd3c422fa3aee359542cae7
Clover <blazelion3@gmail.com>
2019-02-09 14:27:18 -0500

    added mod check_pcpermits.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate image;
 extern crate reqwest;
 
 mod check_cryo;
+mod check_pcpermits;
 mod lhc_status_options;
 mod utils;
 mod web;
@@ -42,34 +43,7 @@ fn main() {
 fn select_option(n: u32) {
     match n {
         1 => check_cryo::check_cryo_status(),
-        3 => check_60_amp_status(),
+        3 => check_pcpermits::check_60_amp_status(),
         _ => println!("Select a valid option"),
     }
-}
-
-fn check_60_amp_status() {
-    web::get_image("https://vistar-capture.web.cern.ch/vistar-capture/lhc2.png");
-    let img = image::open("./test.png").expect("Unable to open image");
-    let coords = [
-        (108, 403), //S12
-        (203, 403), //S23
-        (297, 403), //S34
-        (392, 402), //S45
-        (498, 402), //S56
-        (595, 402), //S67
-        (688, 403), //S78
-        (772, 402), //S81
-    ];
-
-    let pixels = utils::get_pixels(&coords, img);
-    let all_good = pixels.len() * 255;
-    let sum_of_good_cyrostats = utils::get_sum_of_good_cryostats(pixels);
-
-    if all_good == sum_of_good_cyrostats {
-        println!("All PCPermits are up");
-    } else {
-        println!("A PCPermit is down");
-    }
-
-    utils::clean_up_image().expect("Unable to clean up image");
 }
