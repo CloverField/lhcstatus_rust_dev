b39364e90070b9573cc777dfbea6915ab08ab480
Clover <blazelion3@gmail.com>
2019-02-09 14:26:43 -0500

    moved check_60_amp_status to check_pcpermits.

---
+++ src/check_pcpermits.rs
@@ -0,0 +1,31 @@
+extern crate image;
+
+use crate::utils;
+use crate::web;
+
+pub fn check_60_amp_status() {
+    web::get_image("https://vistar-capture.web.cern.ch/vistar-capture/lhc2.png");
+    let img = image::open("./test.png").expect("Unable to open image");
+    let coords = [
+        (108, 403), //S12
+        (203, 403), //S23
+        (297, 403), //S34
+        (392, 402), //S45
+        (498, 402), //S56
+        (595, 402), //S67
+        (688, 403), //S78
+        (772, 402), //S81
+    ];
+
+    let pixels = utils::get_pixels(&coords, img);
+    let all_good = pixels.len() * 255;
+    let sum_of_good_cyrostats = utils::get_sum_of_good_cryostats(pixels);
+
+    if all_good == sum_of_good_cyrostats {
+        println!("All PCPermits are up");
+    } else {
+        println!("A PCPermit is down");
+    }
+
+    utils::clean_up_image().expect("Unable to clean up image");
+}
