7cb4ac8c96d4b219ad72193094fef0b284d0c996
Clover <blazelion3@gmail.com>
2019-02-10 18:42:05 -0500

    updated get_image to use temp dir. No more cleaning up required.

--- src/web.rs
+++ src/web.rs
@@ -1,10 +1,38 @@
+extern crate image;
 extern crate reqwest;
+extern crate tempdir;
 
+use std::io::copy;
 use std::fs::File;
 use std::io;
+use tempdir::TempDir;
 
-pub fn get_image(url: &str) {
-    let mut resp = reqwest::get(url).expect("request failed");
-    let mut out = File::create("./test.png").expect("failed to create file");
-    io::copy(&mut resp, &mut out).expect("failed to copy content");
+pub fn get_image(url: &str) -> io::Result<image::DynamicImage> {
+    let tmp_dir = TempDir::new("example")?;
+    let target = url;
+    let mut response = reqwest::get(target).expect("request failed");
+
+    let mut dest = {
+        let fname = response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() { None } else { Some(name) })
+            .unwrap_or("tmp.bin");
+
+        let fname = tmp_dir.path().join(fname);
+        File::create(fname)?
+    };
+
+    copy(&mut response, &mut dest)?;
+
+    let fname = response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() { None } else { Some(name) })
+            .unwrap_or("tmp.bin");
+
+    let fname = tmp_dir.path().join(fname);
+    Ok(image::open(fname).expect("Unable to open image"))
 }
